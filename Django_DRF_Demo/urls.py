28114837c69894d6a0196be710803bfce20cf9de
Rohit Gaikwad <rgaikwad1998@gmail.com>
2023-09-23 01:13:48 +0530

    Updated Files

--- Django_DRF_Demo/urls.py
+++ Django_DRF_Demo/urls.py
@@ -5,18 +5,18 @@ The `urlpatterns` list routes URLs to views. For more information please see:
 Examples:
 Function views
     1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
+    2. Add a URL to urlpatterns:  path('', views.Student, name='Student')
 Class-based views
     1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
+    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='Student')
 Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
 from django.urls import path
-from home import views
-from home import view_drf
+from Student import views
+from Student import view_drf
 
 urlpatterns = [
     path('admin/', admin.site.urls),
